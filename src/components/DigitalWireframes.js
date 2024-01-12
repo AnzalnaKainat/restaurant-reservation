76b76fc8c034153d90957138a54f06bcf8780d56
AnzalnaKainat <anzalnakainat@gmail.com>
2024-01-12 20:59:39 +0500

    Changes

--- src/components/DigitalWireframes.js
+++ src/components/DigitalWireframes.js
@@ -15,7 +15,7 @@ const DigitalWireframes = () =>{
 
   {/* Digital Wireframes Mobile responsive */}
   <section className="persona digital-wireframe-mobile">
-    <h2>Paper Wireframes For Mobile Screen</h2>
+    <h2>Digital Wireframes For Mobile Screen</h2>
     <p>Spending time on paper wireframes for the smaller screen version was essential in ensuring that the digital wireframes effectively addressed user concerns. The focus on the home screen remained the same, emphasizing solutions for pain points like language selection and restaurant categories.</p>
     <div className="images">
     <img src={digitalHomepageMobile} alt="" />
